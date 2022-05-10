1345c655430cd68c1a8c06512447edbf4dbcce62
Maurizio Proietto <proiettomaurizio97@gmail.com>
2022-05-10 22:25:19 +0200

    Added routing

---
+++ score.php
@@ -0,0 +1,48 @@
+<?php
+
+$challenge = $_GET['challenge'];
+
+$folder    = 'polls/' . $challenge;
+$data = [];
+$scan = scandir("$folder/");
+
+$i = 0;
+foreach($scan as $file) {
+    if (!is_dir("$folder/$file")) {
+       $content = file_get_contents("$folder/$file");
+       $content = json_decode($content);
+       $array = get_object_vars($content);
+       $array = points($array);
+
+       foreach ($array as $key => $value) {
+          $people [$key] += $value;
+       };
+    }
+ }
+$ciao = 'ciaooooo';
+// echo 'aaa';
+ function points(array $array) {
+  foreach ($array as $key => $value) {
+      if($value == 1) {
+        $array[$key] = 7;
+      }
+      if($value == 2) {
+        $array[$key] = 5;
+      }
+      if($value == 3) {
+        $array[$key] = 4;
+      }
+      if($value == 4) {
+        $array[$key] = 3;
+      }
+      if($value == 5) {
+        $array[$key] = 2;
+      }
+      if($value >= 6) {
+        $array[$key] = 1;
+      }
+  }
+
+  return $array;
+ }
+ ?>
