0783c548cafbeee29cf2106a91c2f1c3e46a409d
Maurizio Proietto <proiettomaurizio97@gmail.com>
2022-05-11 16:44:54 +0200

    refactoring

--- host.php
+++ host.php
@@ -4,9 +4,9 @@ if (strpos($actual_link, 'localhost')) {
     $home = $actual_link . '/poll-rank/index.php?route=home';
     $scoreboard = $actual_link . '/poll-rank/index.php?route=scorboard';
 } else {
-    $home = $actual_link . 'index.php?route=home';
+    $home = $actual_link . '/index.php?route=home';
     $scoreboard = $actual_link . '/index.php?route=scorboard';
 }
 
-define(HOME, $home);
-define(SCOREBOARD, $scoreboard);
+define('HOME', $home);
+define('SCOREBOARD', $scoreboard);
