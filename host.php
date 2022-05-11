6e3b87606b70540b945013a37d11f3931bff34e1
Maurizio Proietto <proiettomaurizio97@gmail.com>
2022-05-11 16:36:31 +0200

    added route for pipeline

--- host.php
+++ host.php
@@ -1,11 +1,11 @@
 <?php
 $actual_link = "http://$_SERVER[HTTP_HOST]";
 if (strpos($actual_link, 'localhost')) {
-    $home = $actual_link . "/poll-rank/index.php?route=home";
-    $scoreboard = $actual_link . "/poll-rank/index.php?route=scorboard";
+    $home = $actual_link . '/poll-rank/index.php?route=home';
+    $scoreboard = $actual_link . '/poll-rank/index.php?route=scorboard';
 } else {
-    $scoreboard = $actual_link;
-    $home = $actual_link;
+    $home = $actual_link . 'index.php?route=home';
+    $scoreboard = $actual_link . '/index.php?route=scorboard';
 }
 
 define(HOME, $home);
