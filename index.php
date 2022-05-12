38805e954c92e99d57a7f0f290dc84ffa5c57158
Valerio <87488679+FLUlul@users.noreply.github.com>
2022-05-12 14:44:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,11 +24,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="js/script.js" defer></script>
-    <title>Poll Rank</title>
+    <title>Poll Score</title>
 </head>
 <body>
     <h1>
-        POLL RANK
+        POLL SCORE
     </h1>
     <div class="container">
     <?php include($loader->view_path); ?>
